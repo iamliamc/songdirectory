cd70e3bfea1eeb8ee8ad361dd5bc76c80c42de90
Liam Considine <ljc3179@gmail.com>
2016-06-18 17:12:18 +0000

    Can now delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,13 +7,33 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id]) 
   end
   
+  def edit
+    @comments = Comment.find(params[:id])
+  end
+  
+  def update
+    @comments = Comment.find(params[:id])
+    if @comments.update_attributes(comment_params)
+      flash[:success] = "Comment updated"
+      redirect_to '/songs/' + @comments.song_id.to_s
+    else
+      render 'edit'
+    end
+  end
+  
   def create
     @comments = Comment.new(comment_params) 
     if @comments.save 
-      redirect_to '/songs/' 
+      redirect_to '/songs/' + @comments.song_id.to_s 
     else 
       render 'new' 
     end
+  end
+  
+  def destroy
+    @comments = Comment.find(params[:id])
+    @comments.destroy
+    redirect_to '/songs/'
   end
   
   private 
