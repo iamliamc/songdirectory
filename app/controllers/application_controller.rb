d7e7a193794c6ca02a0c83ef9d21463858ab2df4
Liam Considine <ljc3179@gmail.com>
2016-06-26 16:14:12 +0000

    Added error message for trying to access pages without logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,20 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id] 
   end
   
-  def require_user 
-    redirect_to '/login' unless current_user
+  #def require_user 
+    #redirect_to '/login' unless current_user
     #Not sure if this will actually pop with current flow control
-    flash[:error] = "You must be logged in to access this section"
+    #flash[:error] = "You must be logged in to access this section"
+  #end
+  
+  def require_user
+    if current_user
+    else
+      #flash[:error] = "You must be logged in to access this section"
+      redirect_to login_path, :flash => { :error => "You must be logged in to access this section" }
+    end
   end
+      
   
   protect_from_forgery with: :exception
 end
